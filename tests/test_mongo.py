20ac744d35bbdf685df2a3b6027def4299da0254
Giacomo <giacomo.licari@gmail.com>
2017-05-24 16:02:41 +0200

    added mongo connection tests

---
+++ tests/test_mongo.py
@@ -0,0 +1,26 @@
+# PyCharm fix
+from __future__ import absolute_import
+import os
+import unittest
+from pymongo import MongoClient
+
+
+class TestMongo(unittest.TestCase):
+    """python -m unittest tests.test_mongo"""
+
+    def __init__(self, *args, **kwargs):
+        super(TestMongo, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.client = MongoClient('mongo', 27017)
+        self.db = self.client.test_db
+
+    def test_connection(self):
+        data = {'name': 'Giacomo'}
+        _id = self.db.test_collection.insert_one(data).inserted_id
+        self.assertIsNotNone(_id)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
