1490e4445bb29fa39157979d0d9e8c0696ae9ff8
Rami Khalil <the.rami.khalil@gmail.com>
2017-06-06 11:38:51 +0200

    Central + Ultimate oracle endpoints

---
+++ gnosisdb/restapi/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers
+from gnosisdb.relationaldb.models import CentralizedOracle
+
--- gnosisdb/settings/local.py
+++ gnosisdb/settings/local.py
@@ -22,7 +22,9 @@ INSTALLED_APPS = (
     'gnosisdb.tests',
     'django_ether_logs',
     'djcelery',
-    'relationaldb'
+    'relationaldb',
+    'rest_framework',
+    'restapi',
 )
 
 MIDDLEWARE_CLASSES = [
@@ -89,8 +91,8 @@ DATABASES = {
         'NAME': 'postgres',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'db',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
