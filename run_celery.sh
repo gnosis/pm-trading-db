c1bf54d99d1404c89b318bd4f181a74d44b01016
Giacomo <giacomo.licari@gmail.com>
2017-06-07 10:32:30 +0200

    run celery worker in background

--- run_celery.sh
+++ run_celery.sh
@@ -3,7 +3,9 @@
 echo "==> call run_celery.sh <=="
 sleep 10
 cd gnosisdb/
-celery -A gnosisdb.apps worker -Q default -n default@%h --loglevel debug --workdir="$PWD" -c 1
+echo "==> run worker <=="
+celery -A gnosisdb.apps worker -Q default -n default@%h --loglevel debug --workdir="$PWD" -c 1 &
 sleep 10
+echo "==> run beat <=="
 celery -A gnosisdb.apps beat -S djcelery.schedulers.DatabaseScheduler --loglevel debug --workdir="$PWD"
 echo "==> run_celery.sh done <=="
