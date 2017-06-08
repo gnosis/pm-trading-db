a75b9871856d24c731b610f4736988977cc7a6d0
Giacomo <giacomo.licari@gmail.com>
2017-06-08 12:22:44 +0200

    added API tests for ultimate and centralized oracles

--- gnosisdb/restapi/urls.py
+++ gnosisdb/restapi/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^centralized-oracles/$', views.CentralizedOracleListView.as_view()),
-    url(r'^centralized-oracles/(?P<addr>[a-fA-F0-9]+)/$', views.CentralizedOracleFetchView.as_view()),
-    url(r'^ultimate-oracles/$', views.UltimateOracleListView.as_view()),
-    url(r'^ultimate-oracles/(?P<addr>[a-fA-F0-9]+)/$', views.UltimateOracleFetchView.as_view()),
+    url(r'^centralized-oracles/$', views.CentralizedOracleListView.as_view(), name='centralized-oracles'),
+    url(r'^centralized-oracles/(?P<addr>[a-fA-F0-9]+)/$', views.CentralizedOracleFetchView.as_view(), name='centralized-oracles-by-address'),
+    url(r'^ultimate-oracles/$', views.UltimateOracleListView.as_view(), name='ultimate-oracles'),
+    url(r'^ultimate-oracles/(?P<addr>[a-fA-F0-9]+)/$', views.UltimateOracleFetchView.as_view(), name='ultimate-oracles-by-address'),
     url(r'^events/$', views.EventListView.as_view()),
     url(r'^events/(?P<addr>[a-fA-F0-9]+)/$', views.EventFetchView.as_view()),
     url(r'^markets/$', views.MarketListView.as_view()),
