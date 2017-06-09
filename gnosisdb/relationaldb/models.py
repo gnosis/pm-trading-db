21c67d043283e0c88dc05a6df7b9fcdff4e09325
Denis Granha <denisgranha@gmail.com>
2017-06-09 12:09:13 +0200

    added comments to code to refactor code

--- gnosisdb/relationaldb/models.py
+++ gnosisdb/relationaldb/models.py
@@ -8,6 +8,7 @@ from django.contrib.postgres.fields import ArrayField
 # Abstract Contract Structure
 class Contract(models.Model):
     address = models.CharField(max_length=20, primary_key=True)
+    # todo rename to factory
     factory_address = models.CharField(max_length=20)
     creator = models.CharField(max_length=20)
     creation_date = models.DateTimeField()
