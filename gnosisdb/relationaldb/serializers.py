69e4e535416dc5f3782a4d0733034fef580a4c81
Denís Graña <denisgranha@gmail.com>
2017-06-12 12:07:44 +0200

    testing serializer

--- gnosisdb/relationaldb/serializers.py
+++ gnosisdb/relationaldb/serializers.py
@@ -4,22 +4,62 @@ from relationaldb import models
 
 # Declare basic fields, join params on root object and
 class ContractSerializer(serializers.BaseSerializer):
-    def to_internal_value(self, data):
-        factory = data.get('address')
+    class Meta:
+        fields = ('factory', 'address', 'creator', 'creation_date', 'creation_block', )
 
-        internal_value = {
-            'factory': factory
+    factory = serializers.CharField(max_length=22)  # included prefix
+    creation_date = serializers.DateTimeField()
+    creation_block = serializers.IntegerField()
+
+    def __init__(self, *args, **kwargs):
+        self.block = kwargs.pop('block')
+
+        data = kwargs.pop('data')
+        # Event params moved to root object
+        new_data = {
+            'address': data[u'address'],
+            'creation_date': self.block.get('timestamp'),
+            'creation_block': self.block.get('number')
         }
 
         for param in data.get('params'):
-            internal_value[param[u'name']] = param[u'value']
+            new_data[param[u'name']] = param[u'value']
+
+        kwargs['data'] = new_data
+        super(ContractSerializer, self).__init__(*args, **kwargs)
+
+    # def to_internal_value(self, data):
+    #     factory = data.get('address')
+    #     if not factory:
+    #         raise serializers.ValidationError('Invalid event, factory address needed')
+    #
+    #     internal_value = {
+    #         'factory': factory,
+    #         'creation_date': self.block.get('timestamp'),
+    #         'creation_block': self.block.get('number')
+    #     }
+    #
+    #     return internal_value
 
 
-class CentralizedOracleSerializer(serializers.ModelSerializer):
+class CentralizedOracleSerializer(ContractSerializer, serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        s = super(CentralizedOracleSerializer, self).__init__(*args, **kwargs)
+        pass
+    
     class Meta:
         model = models.CentralizedOracle
-        extra_kwargs = {'block': {'write_only': True}}
+        fields = ContractSerializer.Meta.fields + ('owner', )
 
-    factory = serializers.CharField(max_length=22) # included prefix
-    address = serializers.CharField(max_length=22, source='centralizedOracleCreation')
-    creator = serializers.CharField(max_length=22, source='creator')
+    address = serializers.CharField(max_length=22, source='centralizedOracle')
+    creator = serializers.CharField(max_length=22)
+    owner = serializers.CharField(max_length=22, source='creator')
+
+    def validate(self, attrs):
+        pass
+
+    # ipfs_hash
+
+    # def to_internal_value(self, data):
+    #     i = super(CentralizedOracleSerializer, self).to_internal_value(data)
+    #     return i
--- gnosisdb/relationaldb/tests/test_serializers.py
+++ gnosisdb/relationaldb/tests/test_serializers.py
@@ -1,6 +1,36 @@
 from unittest import TestCase
 from relationaldb.factories import CentralizedOracleFactory
+from relationaldb.serializers import CentralizedOracleSerializer
+
 
 class TestSerializers(TestCase):
     def test_centralized_oracle(self):
-        oracle = CentralizedOracleFactory
+        oracle = CentralizedOracleFactory()
+
+        block = {
+            'number': oracle.creation_block,
+            'creationDate': oracle.creation_date
+        }
+
+        oracle_event = {
+            'address': oracle.factory,
+            'params': [
+                {
+                    'name': 'creator',
+                    'value': oracle.creator
+                },
+                {
+                    'name': 'centralizedOracle',
+                    'value': oracle.address
+                },
+                # {
+                #     'name': 'ipfsHash',
+                #     'value': oracle.event_description
+                # }
+            ]
+        }
+
+        s = CentralizedOracleSerializer(data=oracle_event, block=block)
+        self.assertTrue(s.is_valid(), s.errors)
+        instance = s.save()
+        self.assertIsNotNone(instance)
