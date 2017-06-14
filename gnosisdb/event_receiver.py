6217646450fa63c7f3de7a019fad823f02cf0ecf
Giacomo <giacomo.licari@gmail.com>
2017-06-14 13:42:25 +0200

    moved event_receiver under eth/

--- gnosisdb/event_receiver.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class AbstractEventReceiver(object):
-    """Abstract EventReceiver class."""
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def save(self, decoded_event): pass
