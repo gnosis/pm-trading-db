5b536ca7a56d48e68c36d300ea8bb0427e78e2d8
Rami Khalil <the.rami.khalil@gmail.com>
2017-06-16 16:26:57 +0200

    Unused util

--- gnosisdb/utils.py
+++ gnosisdb/utils.py
@@ -1,24 +1,23 @@
-from functools import wraps
-from rest_framework.response import Response
-from rest_framework import status
-import json
-
-
-def limit_content_length(max_length):
-    """Rejects requests whose content oversize the maximum allowed
-
-    Args:
-        max_length: maximum content-length allowed
-    """
-    def decorator(f):
-        @wraps(f)
-        def wrapper(instance, request,*args, **kwargs):
-            content_length = len(json.dumps(request.data))
-            if content_length is not None and content_length > max_length:
-                return Response(status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, data={})
-            return f(instance, request, *args, **kwargs)
-        return wrapper
-    return decorator
+# TODO Delete this block
+# from functools import wraps
+# from rest_framework.response import Response
+# from rest_framework import status
+# import json
+# def limit_content_length(max_length):
+#     """Rejects requests whose content oversize the maximum allowed
+#
+#     Args:
+#         max_length: maximum content-length allowed
+#     """
+#     def decorator(f):
+#         @wraps(f)
+#         def wrapper(instance, request,*args, **kwargs):
+#             content_length = len(json.dumps(request.data))
+#             if content_length is not None and content_length > max_length:
+#                 return Response(status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, data={})
+#             return f(instance, request, *args, **kwargs)
+#         return wrapper
+#     return decorator
 
 
 def singleton(clazz):
