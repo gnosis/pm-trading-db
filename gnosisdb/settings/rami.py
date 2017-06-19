f2615b1f5dd23590fd4eaaed5d80fa7ef645c03f
Rami Khalil <the.rami.khalil@gmail.com>
2017-06-19 13:57:57 +0200

    ABI Loader in rami settings

--- gnosisdb/settings/rami.py
+++ gnosisdb/settings/rami.py
@@ -1,4 +1,5 @@
 from gnosisdb.settings.local import *
+from gnosisdb.compiled_contracts import AbiLoader
 import json
 
 """
@@ -15,30 +16,37 @@ GNOSISDB_CONTRACTS = [
     {
         'ADDRESSES': ['0x254dffcd3277c0b1660f6d42efbb754edababc2b'],
         'ADDRESSES_GETTER': '',
-        'EVENT_ABI': json.loads('[{"inputs": [{"type": "bytes", "name": "ipfsHash"}], "constant": false, "name": "createCentralizedOracle", "payable": false, "outputs": [{"type": "address", "name": "centralizedOracle"}], "type": "function"}, {"inputs": [{"indexed": true, "type": "address", "name": "creator"}, {"indexed": false, "type": "address", "name": "centralizedOracle"}, {"indexed": false, "type": "bytes", "name": "ipfsHash"}], "type": "event", "name": "CentralizedOracleCreation", "anonymous": false}]'),
+        'EVENT_ABI': AbiLoader().centralized_oracle_factory(),
         'EVENT_DATA_RECEIVER': 'eth.event_receiver.CentralizedOracleReceiver',
         'NAME': 'Centralized Oracle Factory' # optional
     },
     {
         'ADDRESSES': ['0xc89ce4735882c9f0f0fe26686c53074e09b0d550'],
         'ADDRESSES_GETTER': '',
-        'EVENT_ABI': json.loads('[{"inputs": [{"type": "address", "name": "oracle"}, {"type": "address", "name": "collateralToken"}, {"type": "uint8", "name": "spreadMultiplier"}, {"type": "uint256", "name": "challengePeriod"}, {"type": "uint256", "name": "challengeAmount"}, {"type": "uint256", "name": "frontRunnerPeriod"}], "constant": false, "name": "createUltimateOracle", "payable": false, "outputs": [{"type": "address", "name": "ultimateOracle"}], "type": "function"}, {"inputs": [{"indexed": true, "type": "address", "name": "creator"}, {"indexed": false, "type": "address", "name": "ultimateOracle"}, {"indexed": false, "type": "address", "name": "oracle"}, {"indexed": false, "type": "address", "name": "collateralToken"}, {"indexed": false, "type": "uint8", "name": "spreadMultiplier"}, {"indexed": false, "type": "uint256", "name": "challengePeriod"}, {"indexed": false, "type": "uint256", "name": "challengeAmount"}, {"indexed": false, "type": "uint256", "name": "frontRunnerPeriod"}], "type": "event", "name": "UltimateOracleCreation", "anonymous": false}]'),
+        'EVENT_ABI': AbiLoader().ultimate_oracle_factory(),
         'EVENT_DATA_RECEIVER': 'eth.event_receiver.UltimateOracleReceiver',
         'NAME': 'Ultimate Oracle Factory'
     },
     {
         'ADDRESSES': ['0x5b1869d9a4c187f2eaa108f3062412ecf0526b24'],
         'ADDRESSES_GETTER': '',
-        'EVENT_ABI': json.loads('[{"inputs": [{"type": "address", "name": "collateralToken"}, {"type": "address", "name": "oracle"}, {"type": "int256", "name": "lowerBound"}, {"type": "int256", "name": "upperBound"}], "constant": false, "name": "createScalarEvent", "payable": false, "outputs": [{"type": "address", "name": "eventContract"}], "type": "function"}, {"inputs": [{"type": "bytes32", "name": ""}], "constant": true, "name": "categoricalEvents", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "bytes32", "name": ""}], "constant": true, "name": "scalarEvents", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "collateralToken"}, {"type": "address", "name": "oracle"}, {"type": "uint8", "name": "outcomeCount"}], "constant": false, "name": "createCategoricalEvent", "payable": false, "outputs": [{"type": "address", "name": "eventContract"}], "type": "function"}, {"inputs": [{"indexed": true, "type": "address", "name": "creator"}, {"indexed": false, "type": "address", "name": "categoricalEvent"}, {"indexed": false, "type": "address", "name": "collateralToken"}, {"indexed": false, "type": "address", "name": "oracle"}, {"indexed": false, "type": "uint8", "name": "outcomeCount"}], "type": "event", "name": "CategoricalEventCreation", "anonymous": false}, {"inputs": [{"indexed": true, "type": "address", "name": "creator"}, {"indexed": false, "type": "address", "name": "scalarEvent"}, {"indexed": false, "type": "address", "name": "collateralToken"}, {"indexed": false, "type": "address", "name": "oracle"}, {"indexed": false, "type": "int256", "name": "lowerBound"}, {"indexed": false, "type": "int256", "name": "upperBound"}], "type": "event", "name": "ScalarEventCreation", "anonymous": false}]'),
+        'EVENT_ABI': AbiLoader().event_factory(),
         'EVENT_DATA_RECEIVER': 'eth.event_receiver.EventReceiver',
         'NAME': 'Event Factory'
     },
     {
         'ADDRESSES': ['0x9561c133dd8580860b6b7e504bc5aa500f0f06a7'],
         'ADDRESSES_GETTER': '',
-        'EVENT_ABI': json.loads('[{"inputs": [{"type": "address", "name": "eventContract"}, {"type": "address", "name": "marketMaker"}, {"type": "uint24", "name": "fee"}], "constant": false, "name": "createMarket", "payable": false, "outputs": [{"type": "address", "name": "market"}], "type": "function"}, {"inputs": [{"indexed": true, "type": "address", "name": "creator"}, {"indexed": false, "type": "address", "name": "market"}, {"indexed": false, "type": "address", "name": "eventContract"}, {"indexed": false, "type": "address", "name": "marketMaker"}, {"indexed": false, "type": "uint24", "name": "fee"}], "type": "event", "name": "MarketCreation", "anonymous": false}]'),
+        'EVENT_ABI': AbiLoader().standard_market_factory(),
         'EVENT_DATA_RECEIVER': 'eth.event_receiver.MarketReceiver',
         'NAME': 'Standard Market Factory'
+    },
+    {
+        'ADDRESSES': [],
+        'ADDRESSES_GETTER': 'eth.address_getters.MarketAddressGetter',
+        'EVENT_ABI': AbiLoader().standard_market(),
+        'EVENT_DATA_RECEIVER': 'eth.event_receiver.MarketOrderReceiver',
+        'NAME': 'Standard Markets Buy/Sell/Short Receiver'
     },
 ]
 
