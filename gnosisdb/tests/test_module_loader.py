2603ed66b54666fdcad4ea87bdeafb0084dea6ea
Denis Granha <denisgranha@gmail.com>
2017-06-27 23:28:12 +0200

    fixed test, removed old code

--- gnosisdb/tests/test_module_loader.py
+++ gnosisdb/tests/test_module_loader.py
@@ -17,18 +17,10 @@ class TestModuleLoader(unittest.TestCase):
                 return address in ['0x0', '0x1']
 
 
-        class InvalidAddressesGetter(object):
-            def get_addresses(self):
-                return ['0x0', '0x1']
-
-            def __contains__(self, address):
-                return address in ['0x0', '0x1']
-
         def addresses_getter():
             return ['0x0', '0x1']
 
         setattr(gnosisdb, AddressesGetter.__name__, AddressesGetter)
-        setattr(gnosisdb, InvalidAddressesGetter.__name__, InvalidAddressesGetter)
         setattr(gnosisdb, addresses_getter.__name__, addresses_getter)
 
         self.class_path = 'gnosisdb.AddressesGetter'
@@ -49,11 +41,8 @@ class TestModuleLoader(unittest.TestCase):
 
     def test_getting_addresses_utils_function(self):
         clazz_addresses = import_string(self.class_path)().get_addresses()
-        # invalid_clazz_addresses = addresses_getter(self.invalid_class_path)
-        func_addresses = import_string(self.func_path)().get_addresses()
 
         self.assertEquals(len(clazz_addresses), 2)
-        self.assertEquals(len(func_addresses), 2)
 
         with self.assertRaises(ImportError):
             import_string(self.invalid_class_path)
@@ -62,6 +51,3 @@ class TestModuleLoader(unittest.TestCase):
 
         with self.assertRaises(ImportError):
             import_string(invalid_module_path)
-
-        with self.assertRaises(ImportError):
-            import_string(2)
