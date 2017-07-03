d4e611f758c9a840649626dca176e918186bf6b2
Giacomo <giacomo.licari@gmail.com>
2017-07-03 16:45:37 +0200

    celery now depends on db

--- run_celery.sh
+++ run_celery.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+
+until psql -h "db" -U "postgres" -c '\l'; do
+  >&2 echo "GnosisDB database is unavailable - sleeping"
+  sleep 1
+done
+
+
 if [ -f "$HOME/var/run/celery/celerybeat.pid" ]; then
     echo "==> Removing celerybeat.pid..."
 	rm "$HOME/var/run/celery/celerybeat.pid"
 	echo "==> celerybeat.pid removed"
 fi
 
-# wait for RabbitMQ server to start
+# wait for RabbitMQ server and Postgres to start
 echo "==> call run_celery.sh <=="
-sleep 10
+
 cd gnosisdb/
 echo "==> run worker <=="
 celery -A gnosisdb.apps worker -Q default -n default@%h --loglevel debug --workdir="$PWD" -c 1 &
--- run_django.sh
+++ run_django.sh
@@ -1,5 +1,5 @@
 echo "==> Migrating Django <=="
-cd gnosisdb/
+# cd gnosisdb/
 python manage.py migrate
 echo "==> Starting Django Server <=="
 python manage.py runserver 0.0.0.0:8000
