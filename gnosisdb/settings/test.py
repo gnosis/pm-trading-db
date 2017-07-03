68b851747aa4b4ef7fc8733ca4fa908985f026cb
Denís Graña <denisgranha@gmail.com>
2017-07-03 18:18:11 +0200

    travisci conf

--- gnosisdb/settings/test.py
+++ gnosisdb/settings/test.py
@@ -1,6 +1,21 @@
 from gnosisdb.settings.local import *
+import os
+
 
 # Insert your rpc config here
 ETHEREUM_NODE_HOST= 'localhost'
 ETHEREUM_NODE_PORT = 8545
 ETHEREUM_NODE_SSL = 0
+
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
