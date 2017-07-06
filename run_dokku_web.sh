185a223e779264dc1982f319890e8955796248cb
Giacomo <giacomo.licari@gmail.com>
2017-07-06 17:34:10 +0200

    updated run_dokku_web

--- run_dokku_web.sh
+++ run_dokku_web.sh
@@ -1,3 +1,6 @@
+echo "==> Migrating Django models ... "
 python gnosisdb/manage.py migrate --noinput
+echo "==> Collecting statics ... "
 python gnosisdb/manage.py collectstatic
+echo "==> Running Gunicorn ... "
 gunicorn --pythonpath "$PWD/gnosisdb" wsgi:application --log-file=- --error-logfile=- --access-logfile '-' --log-level info
