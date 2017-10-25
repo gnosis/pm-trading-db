f597786517b7c40529f5b5139a4a4855ef9c5acb
Giacomo <giacomo.licari@gmail.com>
2017-10-25 12:37:02 +0200

    Declared production settings for olympia

--- gnosisdb/settings/olympia.py
+++ gnosisdb/settings/olympia.py
@@ -1,18 +1,78 @@
-from gnosisdb.settings.local import *
+from gnosisdb.settings.base import *
 from gnosisdb.chainevents.abis import abi_file_path, load_json_file
+import os
 
-ETHEREUM_NODE_HOST= 'rinkeby.infura.io'
-ETHEREUM_NODE_PORT = 443
-ETHEREUM_NODE_SSL = 1
+CELERY_SEND_TASK_ERROR_EMAILS = False
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
+WSGI_APPLICATION = 'wsgi.application'
+
+INSTALLED_APPS += ("gunicorn", )
+
+if DEBUG is False:
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+ALLOWED_HOSTS = ['.gnosis.pm', '127.0.0.1', 'localhost']
+
+# ------------------------------------------------------------------------------
+# EMAIL CONFIGURATION
+# ------------------------------------------------------------------------------
+EMAIL_HOST=os.environ['EMAIL_HOST']
+EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER=os.environ['EMAIL_HOST_USER']
+EMAIL_PORT=os.environ['EMAIL_PORT']
+EMAIL_USE_TLS = True
+EMAIL_SUBJECT_PREFIX = os.environ['EMAIL_SUBJECT_PREFIX']
+DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+EMAIL_LOG_BACKEND = 'email_log.backends.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+ADMINS = (
+    ('Giacomo', 'giacomo.licari@gnosis.pm'),
+    ('Denis', 'denis@gnosis.pm'),
+    ('Stefan', 'stefan@gnosis.pm'),
+)
+
+# ------------------------------------------------------------------------------
+# ETHEREUM
+# ------------------------------------------------------------------------------
+ETHEREUM_NODE_HOST = os.environ['ETHEREUM_NODE_HOST']
+ETHEREUM_NODE_PORT = os.environ['ETHEREUM_NODE_PORT']
+ETHEREUM_NODE_SSL = bool(int(os.environ['ETHEREUM_NODE_SSL']))
+
+# ------------------------------------------------------------------------------
 # IPFS
-# You can choose between ipfs container address and 'https://ipfs.infura.io'
-IPFS_HOST = 'https://ipfs.infura.io'
-IPFS_PORT = 5001
+# ------------------------------------------------------------------------------
+IPFS_HOST = os.environ['IPFS_HOST']
+IPFS_PORT = os.environ['IPFS_PORT']
 
-# LMSR Market Maker Address
-LMSR_MARKET_MAKER = '9561c133dd8580860b6b7e504bc5aa500f0f06a7'
+# ------------------------------------------------------------------------------
+# RABBIT MQ
+# ------------------------------------------------------------------------------
+RABBIT_HOSTNAME = os.environ['RABBIT_HOSTNAME']
+RABBIT_USER = os.environ['RABBIT_USER']
+RABBIT_PASSWORD = os.environ['RABBIT_PASSWORD']
+RABBIT_PORT = os.environ['RABBIT_PORT']
+RABBIT_QUEUE = os.environ['RABBIT_QUEUE']
+BROKER_URL = 'amqp://{user}:{password}@{hostname}:{port}/{queue}'.format(
+    user=RABBIT_USER,
+    password=RABBIT_PASSWORD,
+    hostname=RABBIT_HOSTNAME,
+    port=RABBIT_PORT,
+    queue=RABBIT_QUEUE
+)
 
+# ------------------------------------------------------------------------------
+# LMSR MARKET MAKER Address
+# ------------------------------------------------------------------------------
+LMSR_MARKET_MAKER = os.environ['LMSR_MARKET_MAKER']
+
+# ------------------------------------------------------------------------------
+# GNOSIS ETHEREUM CONTRACTS
+# ------------------------------------------------------------------------------
 # GnosisDB Contract Addresses
 ETH_EVENTS = [
     {
@@ -82,3 +142,17 @@ ETH_EVENTS = [
         'NAME': 'Ultimate Oracle Instances'
     },
 ]
+
+# ------------------------------------------------------------------------------
+# GNOSIS ETHEREUM CONTRACTS
+# ------------------------------------------------------------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['DATABASE_PORT'],
+    }
+}
