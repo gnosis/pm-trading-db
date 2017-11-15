432fec46cc5179948510e86249d9a5e2eea02049
Giacomo <giacomo.licari@gmail.com>
2017-11-15 19:46:33 +0100

    Updated run_celery.sh to work with Celery 4.1

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -21,5 +21,5 @@ echo "==> run worker <=="
 celery -A gnosisdb.apps worker -Q default -n default@%h --loglevel debug --workdir="$PWD" -c 1 &
 sleep 10
 echo "==> run beat <=="
-celery -A gnosisdb.apps beat -S djcelery.schedulers.DatabaseScheduler --loglevel debug --workdir="$PWD" --pidfile="$HOME/var/run/celery/celerybeat.pid"
+celery -A gnosisdb.apps beat -S django_celery_beat.schedulers:DatabaseScheduler --loglevel debug --workdir="$PWD" --pidfile=$HOME/celerybeat.pid
 echo "==> run_celery.sh done <=="
