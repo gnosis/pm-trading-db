9bcbda776af55fa16856ab4f0547aba414213789
Denis Granha <denisgranha@gmail.com>
2017-11-15 19:53:37 +0100

    removed unneeded property in wsgy file causing error with gunicorn

--- gnosisdb/wsgi.py
+++ gnosisdb/wsgi.py
@@ -11,7 +11,6 @@ from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
-os.environ["CELERY_LOADER"] = "django"
 
 _application = get_wsgi_application()
 
