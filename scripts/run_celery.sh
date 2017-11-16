c8c488b188f93cf68ca8711ce5f9f371743f1d7b
Denis Granha <denisgranha@gmail.com>
2017-11-16 16:49:34 +0100

    modified run_celery script

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-until psql -h "db" -U "postgres" -c '\l'; do
-  >&2 echo "GnosisDB database is unavailable - sleeping"
-  sleep 1
-done
+database_status="$(pg_isready -h $DATABASE_HOST -U $DATABASE_USER -d $DATABASE_NAME)"
+
+case "$database_status" in
+  *$ok_response*) >&2 echo "Database available" ;;
+  *       )  >&2 echo "GnosisDB database is unavailable - check database status" && exit 1;;
+esac
 
 
 if [ -f "$HOME/var/run/celery/celerybeat.pid" ]; then
