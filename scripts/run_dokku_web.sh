dcc29826a2b0ab9f412390d30641be3a66e3acb8
Denis Granha <denisgranha@gmail.com>
2017-11-17 18:06:02 +0100

    use gevent worker for gunicorn instead of sync one

--- scripts/run_dokku_web.sh
+++ scripts/run_dokku_web.sh
@@ -4,4 +4,4 @@ python gnosisdb/manage.py migrate --noinput
 echo "==> Collecting statics ... "
 python gnosisdb/manage.py collectstatic
 echo "==> Running Gunicorn ... "
-gunicorn --pythonpath "$PWD/gnosisdb" wsgi:application --log-file=- --error-logfile=- --access-logfile '-' --log-level info -b 0.0.0.0
+gunicorn --pythonpath "$PWD/gnosisdb" wsgi:application --log-file=- --error-logfile=- --access-logfile '-' --log-level info -b 0.0.0.0 --worker-class gevent
