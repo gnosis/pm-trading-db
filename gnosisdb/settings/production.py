5ef5f87e2124ada0f6d1934878fbe07e7942b207
Denis Granha <denisgranha@gmail.com>
2017-12-07 12:24:09 +0100

    ETH_PROCESS_BLOCK default to 100 in production

--- gnosisdb/settings/production.py
+++ gnosisdb/settings/production.py
@@ -10,6 +10,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 WSGI_APPLICATION = 'wsgi.application'
 
 INSTALLED_APPS += ("gunicorn", )
+ETH_PROCESS_BLOCKS = os.environ.get('ETH_PROCESS_BLOCKS', '100')
+
 if DEBUG is False:
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
