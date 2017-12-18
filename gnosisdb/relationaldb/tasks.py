e0496404e4cf9fce2a4cb16428eeeac40b88ef8d
Denis Granha <denisgranha@gmail.com>
2017-12-18 17:24:18 +0100

    gnosisdb task dump

---
+++ gnosisdb/relationaldb/tasks.py
@@ -0,0 +1,28 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.core.mail import mail_admins
+import traceback
+from django.core.management import call_command, settings
+
+logger = get_task_logger(__name__)
+
+
+def send_email(message):
+    logger.info('Task error: {}'.format(message))
+    # send email
+    mail_admins('[GnosisDB Error] ', message)
+
+
+@shared_task
+def db_dump():
+    """
+    The task creates a dump of the database
+    """
+    try:
+        call_command('db_dump')
+    except Exception as err:
+        logger.error(str(err))
+        send_email(traceback.format_exc())
+
+
+
