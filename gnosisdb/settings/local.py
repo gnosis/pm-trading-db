76b581214b620ed388583363b07f105aec3ea2f3
Uxio Fuentefria <uxio@gnosis.pm>
2018-02-05 17:53:34 +0100

    Enable Djando Debug Toolbar

--- gnosisdb/settings/local.py
+++ gnosisdb/settings/local.py
@@ -11,3 +11,6 @@ TOURNAMENT_TOKEN = '254dffcd3277c0b1660f6d42efbb754edababc2b'
 ETHEREUM_PRIVATE_KEY = '4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# Django Debug Toolbar config
+INTERNAL_IPS = ['localhost', '127.0.0.1', '172.17.0.1', '172.18.0.1']
