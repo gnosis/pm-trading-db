44a663d5ab08bd2778da8d7592bb46e7264d5870
Uxio Fuentefria <uxio@gnosis.pm>
2018-02-06 10:07:52 +0100

    Disable django debug toolbar in production

--- gnosisdb/settings/local.py
+++ gnosisdb/settings/local.py
@@ -7,6 +7,9 @@ from settings.base import *
 SECRET_KEY = 'testtest'
 DEBUG = True
 
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
 TOURNAMENT_TOKEN = '254dffcd3277c0b1660f6d42efbb754edababc2b'
 ETHEREUM_PRIVATE_KEY = '4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'
 
