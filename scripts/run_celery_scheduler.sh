78210b357c2de9b71d3ec81da749c424e224c725
Uxio Fuentefria <uxio@gnosis.pm>
2018-03-07 13:59:10 +0100

    Fix celery signal management

--- scripts/run_celery_scheduler.sh
+++ scripts/run_celery_scheduler.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-addgroup celery
-useradd -ms /bin/bash celery -g celery
-
 database_status="$(pg_isready -h $DATABASE_HOST -U $DATABASE_USER -d $DATABASE_NAME)"
 
+# wait for RabbitMQ server and Postgres to start
 case "$database_status" in
   *$ok_response*) >&2 echo "Database available" ;;
   *       )  >&2 echo "GnosisDB database is unavailable - check database status" && exit 1;;
 esac
 
-
-if [ -f "$HOME/celerybeat.pid" ]; then
-    echo "==> Removing celerybeat.pid..."
-	rm "$HOME/celerybeat.pid"
-	echo "==> celerybeat.pid removed"
-fi
-
-# wait for RabbitMQ server and Postgres to start
-echo "==> call run_celery.sh <=="
+echo "==> run_celery_scheduler.sh <=="
 
 python manage.py migrate --noinput
 
-shutdown() {
-    echo "Shuting down gracefully celery"
-    kill -3 "$child"
-}
+echo "==> Running Celery beat <=="
 
+# DEBUG set in .env
+if [ "$DEBUG" = True ]; then
+    log_level="debug"
+else
+    log_level="info"
+fi
 
-trap shutdown SIGTERM SIGINT
-
-echo "==> run beat <=="
-celery -A gnosisdb.taskapp beat -S django_celery_beat.schedulers:DatabaseScheduler --loglevel debug --pidfile=$HOME/celerybeat.pid &
-
-child=$!
-wait $!
+exec celery beat -A gnosisdb.taskapp -S django_celery_beat.schedulers:DatabaseScheduler --loglevel $log_level --pidfile=/tmp/celery_beat.pid
