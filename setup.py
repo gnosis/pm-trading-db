97231f6bcff21be11a9dfee9018b0ded4d9c2bf5
Uxio Fuentefria <uxio@gnosis.pm>
2018-03-12 11:01:37 +0100

    Update django-eth-events dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 requirements = [
     'djangorestframework',
-    'django-eth-events==2.0.5',
+    'django-eth-events==2.0.6',
     'ethereum==1.6.1',
     'eth-abi==1.0.0',
     'eth-utils==0.7.4'
