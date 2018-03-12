7ff9d72ae66f0d0a812807ae8789d42305a739ec
Uxio Fuentefria <uxio@gnosis.pm>
2018-03-12 11:02:08 +0100

    Set version 1.0.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name='django_gnosisdb',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(exclude=["*.tests",
                                     "*.tests.*",
                                     "tests.*",
