932b481acbdc300f1fa8e474c508683000f9e586
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-16 18:01:49 +0200

    Open gunicorn in port 8888

--- scripts/run_django.sh
+++
@@ -1,6 +0,0 @@
-echo "==> Migrating Django <=="
-# cd $PWD/gnosisdb
-python manage.py migrate
-python manage.py createcachetable
-echo "==> Starting Django Server <=="
-python manage.py runserver 0.0.0.0:8000
--- scripts/run_web.sh
+++ scripts/run_web.sh
@@ -6,4 +6,4 @@ DOCKER_SHARED_DIR=/gnosis-nginx
 rm -rf $DOCKER_SHARED_DIR/*
 STATIC_ROOT=$DOCKER_SHARED_DIR/staticfiles python manage.py collectstatic
 echo "==> Running Gunicorn ... "
-gunicorn --pythonpath "$PWD" config.wsgi:application --log-file=- --error-logfile=- --access-logfile '-' --log-level info -b unix:$DOCKER_SHARED_DIR/gunicorn.socket --worker-class gevent
+gunicorn --pythonpath "$PWD" config.wsgi:application --log-file=- --error-logfile=- --access-logfile '-' --log-level info -b unix:$DOCKER_SHARED_DIR/gunicorn.socket -b 127.0.0.1:8888 --worker-class gevent
