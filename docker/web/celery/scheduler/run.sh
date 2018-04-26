2bd996e87b2547ca57214e32eecd0418523d0bcb
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-26 13:59:26 +0200

    Move docker scripts

---
+++ docker/web/celery/scheduler/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -euo pipefail
+
+# DEBUG set in .env
+if [ "$DEBUG" = True ]; then
+    log_level="debug"
+else
+    log_level="info"
+fi
+
+echo "==> Running Celery beat <=="
+exec celery beat -A gnosisdb.taskapp -S django_celery_beat.schedulers:DatabaseScheduler --loglevel $log_level --pidfile=/tmp/celery_beat.pid
---
+++ docker/web/celery/worker/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -euo pipefail
+
+# DEBUG set in .env
+if [ "$DEBUG" = True ]; then
+    log_level="debug"
+else
+    log_level="info"
+fi
+
+echo "==> Running Celery worker <=="
+exec celery worker -A gnosisdb.taskapp -Q default --hostname default@%h --loglevel $log_level -c 2 --pidfile=/tmp/celery_worker.pid
