5620b9ef984fd940a3d3b4dc38b539412d9cbc6d
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-27 13:21:27 +0200

    Allow gunicorn access from the outside

--- docker/web/run_web.sh
+++ docker/web/run_web.sh
@@ -11,4 +11,4 @@ rm -rf $DOCKER_SHARED_DIR/*
 STATIC_ROOT=$DOCKER_SHARED_DIR/staticfiles python manage.py collectstatic
 
 echo "==> Running Gunicorn ... "
-gunicorn --pythonpath "$PWD" config.wsgi:application --log-file=- --error-logfile=- --access-logfile '-' --log-level info -b unix:$DOCKER_SHARED_DIR/gunicorn.socket -b 127.0.0.1:8888 --worker-class gevent
+gunicorn --pythonpath "$PWD" config.wsgi:application --log-file=- --error-logfile=- --access-logfile '-' --log-level info -b unix:$DOCKER_SHARED_DIR/gunicorn.socket -b 0.0.0.0:8888 --worker-class gevent
