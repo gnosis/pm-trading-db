ad7059d5fabb92fb258e77b72362c0c3689e4acb
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-27 13:46:14 +0200

    Revert "Use docker cache for deploy"
    This reverts commit 210224b9474cd0f150ff876e52174eae27a184ba.

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    if [ "$1" = "develop" -o "$1" = "master" ]; then
-        docker pull gnosispm/$DOCKERHUB_PROJECT:$1
-        docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1
-    else
-        docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
-    fi
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
     docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
     docker push gnosispm/$DOCKERHUB_PROJECT:$1
 fi
