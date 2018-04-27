74aaf030bd8cb2b5b6f79bb0b06cb5bf52be429f
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-27 13:01:33 +0200

    Fix docker deploy script

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -3,6 +3,6 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
-    docker tag gnosisdb gnosispm/$DOCKERHUB_PROJECT:$1
+    docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
     docker push gnosispm/$DOCKERHUB_PROJECT:$1
 fi
