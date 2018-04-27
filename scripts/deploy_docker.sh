210224b9474cd0f150ff876e52174eae27a184ba
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-27 13:22:00 +0200

    Use docker cache for deploy

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]
-then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+    if [ "$1" = "develop" -o "$1" = "master" ]; then
+        docker pull gnosispm/$DOCKERHUB_PROJECT:$1
+        docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1
+    else
+        docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+    fi
     docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
     docker push gnosispm/$DOCKERHUB_PROJECT:$1
 fi
